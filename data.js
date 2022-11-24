e3f1cf542a9e447f95cbc4582c63378879d55887
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2022-11-23 23:43:39 -0300

    added lib/console

--- data.js
+++ data.js
@@ -3,24 +3,15 @@ const readLine = require('./read-line/build/release/read').read
 const typeOf = require('./typeof')
 
 const data = {
-	write({ values = [], sep = '', end = '' } = {}) {
+	print({ values = [], sep = ' ', end = '\n' } = {}) {
 		process.stdout.write(values.join(sep))
 		process.stdout.write(end)
-	},
-	print({ values = [], sep = ' ', end = '\r\n' } = {}) {
-		data.write({ values, sep, end })
 	},
 	exit({ code } = {}) {
 		process.exit(code)
 	},
 	eval({ string } = {}) {
 		return evalCode(string)
-	},
-	read() {
-		return readLine()
-	},
-	clear() {
-		console.clear()
 	},
 	typeof({ value } = {}) {
 		return typeOf(value)
